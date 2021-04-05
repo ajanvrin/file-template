897f51f0dccdb0319a36270f11e5b757cb6f9af9
Alexandre Janvrin <alexandre.janvrin@reseau.eseo.fr>
2021-04-05 16:20:07 +0200

    removed icecream dependency, added nicer formatting

--- file_template/__main__.py
+++ file_template/__main__.py
@@ -1,10 +1,8 @@
-"""Replaces the keywords in a file with the contents of another file.
-"""
+"""Replaces the keywords in a file with the contents of another file."""
 
 from importlib import resources
 
 import click
-import icecream
 
 with resources.path("file_template", "banner.txt") as banner_path:
     with open(banner_path) as f:
@@ -17,11 +15,10 @@ with resources.path("file_template", "banner.txt") as banner_path:
 @click.argument("file2", type=click.File("r"))
 def main(keyword, file1, file2):
     """Replaces KEYWORD in FILE1 withthe contents of FILE2."""
-    
     click.echo(BANNER, err=True)
 
-    with open(file1) as f:
-        template = f.read()
+    with open(file1) as template_file:
+        template = template_file.read()
 
-    with open(file1, "w") as f:
-        f.write(template.replace(keyword, file2.read()))
+    with open(file1, "w") as result_file:
+        result_file.write(template.replace(keyword, file2.read()))
