cc660677cd77f7a01cda0cfecdcc0a68a7f6595b
Alexandre Janvrin <alexandre.janvrin@reseau.eseo.fr>
2021-04-06 08:14:11 +0200

    Slight change to README

--- README.md
+++ README.md
@@ -16,15 +16,16 @@ pipx install file-template
 
 # CLI Usage
 
+```
 Usage: file-template [OPTIONS] KEYWORD FILE1 FILE2
 
   Replaces KEYWORD in FILE1 withthe contents of FILE2.
 
 Options:
   --help  Show this message and exit.
+```
 
-
-# Authors
+# Author
 
 * Alexandre Janvrin, penetration tester at Beijaflore (https://www.beijaflore.com/en/)
 
--- file_template/__main__.py
+++ file_template/__main__.py
@@ -11,14 +11,12 @@ with resources.path("file_template", "banner.txt") as banner_path:
 
 @click.command()
 @click.argument("keyword")
-@click.argument("file1", type=click.Path())
-@click.argument("file2", type=click.File("r"))
+@click.argument("file1", type=click.File())
+@click.argument("file2", type=click.File())
 def main(keyword, file1, file2):
-    """Replaces KEYWORD in FILE1 withthe contents of FILE2."""
+    """Replaces KEYWORD in FILE1 with the contents of FILE2."""
     click.echo(BANNER, err=True)
 
-    with open(file1) as template_file:
-        template = template_file.read()
+    template = file1.read()
 
-    with open(file1, "w") as result_file:
-        result_file.write(template.replace(keyword, file2.read()))
+    click.echo(template.replace(keyword, file2.read()))
